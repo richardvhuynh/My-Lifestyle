0220d402a94ebe453e79fc0e0a99d5518982d8bc
rvh85695 <Richard.huynh7586@gmail.com>
2026-09-09 22:59:28 -0400

    Start

---
+++ My Lifestyle/My_LifestyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_LifestyleApp.swift
+//  My Lifestyle
+//
+//  Created by Richard Van Huynh on 9/9/26.
+//
+
+import SwiftUI
+
+@main
+struct My_LifestyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
